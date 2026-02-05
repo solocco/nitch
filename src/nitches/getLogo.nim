8c09b47c98b8fcee0e76e30a8996c0c42a2b79c0
solocco <imron.turner@gmail.com>
2026-02-05 12:20:15 +0700

    add void linux logos

--- src/nitches/getLogo.nim
+++ src/nitches/getLogo.nim
@@ -1,10 +1,9 @@
 import
   std/terminal,
   ../assets/logos
-
 func getLogo*(distroId: string): tuple =
   const
-    coloredLogos: array[16, tuple] = [
+    coloredLogos: array[17, tuple] = [  
       (fgRed, nitchLogo),
       (fgBlue, archLogo),
       (fgRed, ubuntuLogo),
@@ -20,54 +19,41 @@ func getLogo*(distroId: string): tuple =
       (fgRed, redhatLogo),
       (fgMagenta, gentooLogo),
       (fgMagenta, endeavourosLogo),
-      (fgBlue, artixLogo)
+      (fgBlue, artixLogo),
+      (fgGreen, voidLogo)  
     ]
-
   case distroId:
   of "arch":
     result = coloredLogos[1]
-
   of "ubuntu":
     result = coloredLogos[2]
-
   of "debian":
     result = coloredLogos[3]
-
   of "fedora":
     result = coloredLogos[4]
-
   of "linuxmint":
     result = coloredLogos[5]
-
   of "Zorin OS":
     result = coloredLogos[6]
-
   of "pop":
     result = coloredLogos[7]
-
   of "manjaro":
     result = coloredLogos[8]
-
   of "opensuse":
     result = coloredLogos[9]
-
   of "slackware":
     result = coloredLogos[10]
-
   of "centos":
     result = coloredLogos[11]
-
   of "redhat":
     result = coloredLogos[12]
-
   of "gentoo":
     result = coloredLogos[13]
-
   of "endeavouros":
     result = coloredLogos[14]
-
   of "artix":
     result = coloredLogos[15]
-
+  of "void":
+    result = coloredLogos[16]
   else:
     result = coloredLogos[0]
